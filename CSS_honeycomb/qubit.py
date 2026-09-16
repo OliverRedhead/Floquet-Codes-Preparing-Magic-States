009c5287e0339f93333f061bb90db06d1a6c2ab8
Oliver Redhead <ollieranga@gmail.com>
2026-09-16 15:41:30 +1000

    updating docstrings, remove __gt__

--- CSS_honeycomb/qubit.py
+++ CSS_honeycomb/qubit.py
@@ -8,7 +8,8 @@ class Qubit:
         self.key = key        
         self.pos = pos
 
-    
+    """dunder methods"""
+
     def __repr__(self) -> str:
         """Return a string representation of the qubit (for debug)"""
         return f"Qubit(position={self.pos}, key={self.key})"
@@ -23,11 +24,8 @@ class Qubit:
         i,x,y = self
         return f"QUBIT_COORDS({x},{y}) {i}"
     
-    def __eq__(self, other: object) -> bool:
-        """
-        Return True if this qubit is equal to another qubit.
-        i.e. if this qubit is in same position with same key
-        """
+    def __eq__(self, other) -> bool:
+        """two qubits are equal if their position and key are equal"""
         if not isinstance(other, Qubit):
             return NotImplemented
         
@@ -35,15 +33,7 @@ class Qubit:
                 and self.pos[1] == self.pos[1]
                 and self.key == other.key)
 
-    def __gt__(self, other:object) -> bool:
-        """assert if this qubit is "greater than" another qubit - compare indices"""
-        if not isinstance(other, Qubit):
-            raise NotImplementedError
-        if self.key > other.key:
-            return True
-        return False
-    
-    def __lt__(self, other:object) -> bool:
+    def __lt__(self, other) -> bool:
         """assert if this qubit is "less than" another qubit - compare indices"""
         if not isinstance(other, Qubit):
             raise NotImplementedError
